77d0cd9fd8f85425521262bb01392f7488c84e5d
Bivas Mandal <bivaswap@users.noreply.github.com>
2019-07-03 16:01:16 +0530

    Update lambda.function.elk.py

--- lambda.function.elk.py
+++ lambda.function.elk.py
@@ -7,18 +7,57 @@ def lambda_handler(event, context):
     x=json.loads(x['Message'])
 
     url = 'http://<logstash ip/host>:8080'
-    data = {
-        'mailD': {
-            'messageId': x['mail']['messageId'],
-            'source': x['mail']['source'],
-            'notificationType': x['notificationType'],
-            'destination': x['mail']['destination'][0]
-        },
-        'sent-at': x['mail']['timestamp']
-    }
     headers = {
         'Content-type': 'application/json',
         'Accept': 'text/plain'
     }
-    r = requests.put(url, data=json.dumps(data), headers=headers)
-    print(r)
+    
+    logstashUsername = '<logstash-username>'
+    logstashPassword = '<logstash-password>'
+    
+    if(x['notificationType'] == 'Delivery'):
+        data = {
+            'mail': {
+                'messageId': x['mail']['messageId'],
+                'notificationType': x['notificationType'],
+                'sender': x['mail']['source'],
+                'recipient': x['delivery']['recipients'],
+                'smtpFeedback': x['delivery']['smtpResponse']
+            },
+            'sent-at': x['mail']['timestamp']
+        }
+
+        r = requests.put(url, data=json.dumps(data), headers=headers, auth=(logstashUsername, logstashPassword))
+        print(r)
+
+    elif(x['notificationType'] == 'Bounce'):
+        data = {
+            'mail': {
+                'messageId': x['mail']['messageId'],
+                'notificationType': x['notificationType'],
+                'sender': x['mail']['source'],
+                'recipient': x['bounce']['bouncedRecipients'][0]['emailAddress'],
+                'smtpFeedback': x['bounce']['bouncedRecipients'][0]['diagnosticCode']
+            },
+            'sent-at': x['mail']['timestamp']
+        }
+        r = requests.put(url, data=json.dumps(data), headers=headers, auth=(logstashUsername, logstashPassword))
+        print(r)
+
+    elif(x['notificationType'] == 'Complaint'):
+        data = {
+            'mail': {
+                'messageId': x['mail']['messageId'],
+                'notificationType': x['notificationType'],
+                'sender': x['mail']['source'],
+                'recipient': x['complaint']['complainedRecipients'][0]['emailAddress'],
+                'smtpFeedback': x['complaint']['complaintFeedbackType']
+            },
+            'sent-at': x['mail']['timestamp']
+        }
+        
+        r = requests.put(url, data=json.dumps(data), headers=headers, auth=(logstashUsername, logstashPassword))
+        print(r)
+        
+    else:
+        print('Nothing to parse')
